1bab5b2aae3bb07df15f434ec6a22c5d413a7a81
Даниил Карнаухов <Tyuiu.KarnaukhovDA@outlook.com>
2026-01-19 22:56:45 +0500

    Add project files.

---
+++ Tyuiu.KarnaukhovDA.Sprint3.Task0.V22.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.KarnaukhovDA.Sprint3.Task0.V22.Lib
+{
+    public class DataService : ISprint3Task0V22
+    {
+        public double GetMultiplySeries(double value, int startValue, int stopValue)
+        {
+            if (startValue > stopValue)
+            {
+                return 1.0; 
+            }
+
+            double product = 1.0;
+            double cosValue = Math.Cos(value);
+
+            for (int k = startValue; k <= stopValue; k++)
+            {
+                
+                product *= (Math.Pow(value, k) + 4) * cosValue;
+            }
+
+            return Math.Round(product, 3);
+        }
+    }
+    }
+}
---
+++ Tyuiu.KarnaukhovDA.Sprint3.Task0.V22/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.KarnaukhovDA.Sprint3.Task0.V22.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 | Выполнил: Карнаухов Д. А. | СМАРТб-25-1";
+        Console.WriteLine("*********************************************************************************************************");
+        Console.WriteLine("* Спринт #3                                                                                             *");
+        Console.WriteLine("* Тема: Оператор цикла for                                                                              *");
+        Console.WriteLine("* Задание #0                                                                                            *");
+        Console.WriteLine("* Вариант #22                                                                                           *");
+        Console.WriteLine("* Выполнил: Карнаухов Даниил Александрович | СМАРТб-25-1                                                *");
+        Console.WriteLine("*********************************************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                                              *");
+        Console.WriteLine("* Написать программу используя цикл for, которая вычисляет произведение ряда по формуле, при a=0,25     *");
+        Console.WriteLine("*********************************************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                      *");
+        Console.WriteLine("*********************************************************************************************************");
+
+        double value = 0.25;
+        int startValue = 1;
+        int stopValue = 8;
+
+        Console.WriteLine("Переменная X = " + value);
+        Console.WriteLine("Старт шага = " + startValue);
+        Console.WriteLine("Конец шага = " + stopValue);
+
+        Console.WriteLine("*********************************************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                            *");
+        Console.WriteLine("*********************************************************************************************************");
+
+        Console.WriteLine("Сумма рада = " + ds.GetMultiplySeries(value, startValue, stopValue));
+        Console.ReadKey();
+
+    }
+}
