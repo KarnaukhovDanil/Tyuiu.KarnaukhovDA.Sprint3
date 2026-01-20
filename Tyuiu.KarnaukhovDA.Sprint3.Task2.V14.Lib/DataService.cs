704890d09fc0de529c7d42633f5a7acefdf3155e
Даниил Карнаухов <Tyuiu.KarnaukhovDA@outlook.com>
2026-01-20 22:50:48 +0500

    Task2

--- Tyuiu.KarnaukhovDA.Sprint3.Task2.V14.Lib/DataService.cs
+++ Tyuiu.KarnaukhovDA.Sprint3.Task2.V14.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.KarnaukhovDA.Sprint3.Task2.V14.Lib
         {
             if (startValue > stopValue)
             {
-                // Меняем местами
+                
                 int temp = startValue;
                 startValue = stopValue;
                 stopValue = temp;
@@ -16,7 +16,7 @@ namespace Tyuiu.KarnaukhovDA.Sprint3.Task2.V14.Lib
             double product = 1.0;
             int k = startValue;
 
-            // Цикл do...while
+            
             do
             {
                 product *= Math.Pow((double)value / k, 3);
